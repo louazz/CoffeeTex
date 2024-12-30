813cebca8b2b90deeb4b701fe80fe27dd9904daf
louazz <louaizaiter@gmail.com>
2024-12-30 19:00:28 +0000

    change

--- backend/src/database/connectDB.ts
+++ backend/src/database/connectDB.ts
@@ -2,7 +2,7 @@ import {MongoClient} from "https://deno.land/x/mongo/mod.ts";
 
 const client = new MongoClient()
 
-await client.connect("mongodb://127.0.0.1:27017");
+await client.connect("mongodb://mongo:27017");
 
 
 console.log("Databse connected");
